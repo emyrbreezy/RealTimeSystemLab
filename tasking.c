fbf551f0b5b44335c5bb43e7921ec1ee42a9fd86
emyrbreezy <63417840+emyrbreezy@users.noreply.github.com>
2020-11-15 13:11:05 +0800

    Update tasking.c
    Updated with editable task duration

--- tasking.c
+++ tasking.c
@@ -8,49 +8,59 @@
 //T1=(4,1.0) T2=(5,1.8) T3=(20,1.0) T4=(20,2.0)
 
 
-//int myArray[17]={4,1,1.8,0,1,0,2,1.8,1,0,1.8,1,0,1,0,1.8,0} 
+//int myArray[17]={4,1,1.8,0,1,0,2,1.8,1,0,1.8,1,0,1,0,1.8,0} //haven't used this
 
 
 float time_accumulated;
 struct timeval start, end;
 
+float task_1_duration=1.0;
+float task_2_duration=1.8;
+float task_3_duration=1.0;
+float task_4_duration=2.0;
+int hyperperiod=20.0;
 
 void task1()		//simple task that runs for 1 sec. 
 {
+
 	printf("Task 1:\n\n");
-	sleep(1);
-	time_accumulated = time_accumulated + 1;
+	printf("Process Duration: %.2f\n",task_1_duration);
+	sleep(task_1_duration);
+	time_accumulated = time_accumulated + task_1_duration;
 }
 
 void task2()		//simple task that runs for 1.8 sec
 {
+	
 	printf("Task 2:\n\n");
-	sleep(1.8);
-	time_accumulated = time_accumulated + 1.8;
+	printf("Process Duration: %.2f\n",task_2_duration);
+	sleep(task_2_duration);
+	time_accumulated = time_accumulated + task_2_duration;
 	
 }
 
 void task3()		//simple task that runs for 1 sec
 {
 	printf("Task 3:\n\n");
-	sleep(1);
-	time_accumulated = time_accumulated + 1;
+	printf("Process Duration: %.2f\n",task_3_duration);
+	sleep(task_3_duration);
+	time_accumulated = time_accumulated + task_3_duration;
 }
 
 
 void task4()		//simple task that runs for 2 sec 
 {
 	printf("Task 4:\n\n");
-	sleep(2);
-	time_accumulated = time_accumulated + 2;
+	printf("Process Duration: %.2f\n",task_4_duration);
+	sleep(task_4_duration);
+	time_accumulated = time_accumulated + task_4_duration;
 }
 
 void delay() //delay for hyperperiod
 
 {	
 	int time_unused;
-	int x = time_accumulated;
-	int hyperperiod = 20;
+
 	
 	time_unused = hyperperiod - time_accumulated;
 	
@@ -166,12 +176,7 @@ while(1)
 	else{
 		delay();}
 	
-
-
 }
-
-  
-
 }
 
 
